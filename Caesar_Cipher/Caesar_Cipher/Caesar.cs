d07efc9f7280987abf3c70af6f3fd612aba7d441
Benjamin Lafever <52665892+Benjamin-Lafever@users.noreply.github.com>
2021-05-28 14:50:08 -0400

    Add files via upload

---
+++ Caesar_Cipher/Caesar_Cipher/Caesar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Caesar_Cipher
+{
+    class Caesar
+    {
+        private static string user_statement { get; set; }
+        private static string encrypted_statement { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Message to become encrypted");
+            user_statement = Console.ReadLine();
+            encrypted_statement = Cipher.Encrypt_Statement(user_statement.ToLower());
+            Console.WriteLine("Here is your encrypted statement: " + encrypted_statement.ToUpper());
+            Console.WriteLine("Would you like to decrypt this statement?");
+
+        }
+    }
+}
---
+++ Caesar_Cipher/Caesar_Cipher/Cipher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Caesar_Cipher
+{
+    static class Cipher
+    {
+        private static string encrypted_User_Statement;
+        static char[] alphabet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        static char[] key = new char[] { 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f' };
+        static int x = 0;
+        
+
+        public static string Encrypt_Statement(string statement) 
+        {
+            char[] statement_array = new char[statement.Length];
+            using(StringReader sr = new StringReader(statement))
+            {
+                sr.Read(statement_array, 0, statement.Length);
+            }
+            for (int i = 0; i < statement_array.Length; i++) 
+            {
+                if(statement_array[i] == ' ')
+                {
+                    encrypted_User_Statement += " ";
+                    goto skip;
+                }
+                for(int y = 0; y < alphabet.Length; y++)
+                {
+                    if (statement_array[i] == alphabet[y])
+                    {
+                        encrypted_User_Statement += key[y];
+                        goto skip;
+                    }
+                }
+            skip:;
+            }
+            return encrypted_User_Statement;
+        }
+    }
+}
