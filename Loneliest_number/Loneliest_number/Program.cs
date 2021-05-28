53356befccc164acc7b3d1d46f12fdac0a5a7b08
Benjamin Lafever <52665892+Benjamin-Lafever@users.noreply.github.com>
2021-05-28 14:51:17 -0400

    Add files via upload

---
+++ Loneliest_number/Loneliest_number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loneliest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            loneliest_num();
+        }
+
+        public static void loneliest_num()
+        {
+            for(int i = 1; i < 1000; i++)
+            {
+                int x = i + 1;
+                if( i == 1)
+                {
+                    Console.WriteLine(i + " is the loneliest number that you'll ever do, " + x + " can be as bad as " + i + " it's the loneiest number since the number " + i + ".\n");
+                }
+                else Console.WriteLine(x + " can be as bad as " + i + " it's the loneiest number since the number " + i + ".\n");
+            }
+        }
+    }
+}
